c8b8f3bca972d98ea09b9e8305ebef86f71e6ec0
Pritom Karmokar <pritomkarmokar99@gmail.com>
2026-08-02 01:17:55 +0600

    feat: one edit distance updated

--- dsa/arrays-strings/one_edit_distance.py
+++ dsa/arrays-strings/one_edit_distance.py
@@ -1,6 +1,6 @@
 # https://algo.monster/liteproblems/161
 # Currently it's very bad solution version for the problem statement. Need to optimize it properly
-def is_one_edit_distance(s: str, t: str) -> bool:
+def is_one_edit_distance_v1(s: str, t: str) -> bool:
     if len(s) == len(t):
         replaceFound = False
         for a, b in zip(s, t):
@@ -23,6 +23,41 @@ def is_one_edit_distance(s: str, t: str) -> bool:
                 counter += 1
 
         return counter == 1
+
+def is_one_edit_distance(s: str, t: str) -> bool:
+    if abs(len(s) - len(t)) > 1:
+        return False
+
+    if len(s) == len(t):
+        return one_edit_replace(s, t)
+    elif len(s) + 1 == len(t):
+        return one_edit_insert(s, t)
+    elif len(s) - 1 == len(t):
+        return one_edit_insert(t, s)
+
+    return False
+
+def one_edit_replace(s: str, t: str) -> bool:
+    counter = 0
+    for ch1, ch2 in zip(s, t):
+        if ch1 != ch2:
+            counter += 1
+
+    return counter == 1
+
+def one_edit_insert(s: str, t: str) -> bool:
+    index1, index2 = 0, 0
+    l1 = len(s)
+    l2 = len(t)
+    while index2 < l2 and index1 < l1:
+        if s[index1] != t[index2]:
+            if index1 != index2:
+                return False
+            index2 += 1
+        else:
+            index1 += 1
+            index2 += 1 
+    return True
 
 s = "ab"
 t = "ad"
